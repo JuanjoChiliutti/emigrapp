f44799ec0bc6f4eb83bb035641f5e23d4f45b357
Giuxpro <albertogiusepp@gmail.com>
2023-01-26 18:56:29 -0300

    middleware for auth done and some folders modifications

--- server/Controllers/CreateUser.js
+++ server/Controllers/CreateUser.js
@@ -1,13 +1,19 @@
+require('dotenv').config()
 const userSchema = require("../Models/users.js");
+const jwt = require("jsonwebtoken");
+const { JWTKEY } = process.env
 
-const createUser = (req, res) => {
+
+const createUser = async(req, res) => {
 
   const user = userSchema(req.body);
-  
-  user
-    .save()
-    .then((data) => res.json(data))
-    .catch((error) => res.json({ message: error }));
+  try {
+    await user.save()
+    const token = jwt.sign({userId: user._id}, JWTKEY)
+    res.send({token})
+  }catch(error) {
+    res.status(401).json({ message: error })
+  }
 };
 
 module.exports = {
--- server/Routes/route.js
+++ server/Routes/route.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const { createUser } = require("../Controllers/CreateUser");
 const { encriptingPass } = require("../Controllers/encriptingPass");
+const { requireToken } = require("../Middleware/requiretoken");
+
 
 const router = express.Router();
 
 
-router.post("/", createUser);
+router.post("/signup", createUser);
 router.post("/login", encriptingPass);
+router.get("/protected",  requireToken);
 
 
 module.exports = router;
