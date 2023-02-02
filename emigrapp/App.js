075b3741f3ce891188a10ce9da05e7cb0e6417b5
Giuxpro <albertogiusepp@gmail.com>
2023-02-02 17:44:09 -0300

    installint and structuring navigations screems and dependency

--- emigrapp/App.js
+++ emigrapp/App.js
@@ -1,95 +1,16 @@
 import React from "react";
-import { StatusBar } from "expo-status-bar";
-import {
-  Alert,
-  ImageBackground,
-  Linking,
-  Pressable,
-  StyleSheet,
-  Text,
-  View,
-} from "react-native";
-import { LinearGradient } from "expo-linear-gradient";
-import image from "./assets/bgemigrapp.jpg";
+import Landing from "./src/components/Landing";
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
+const Stack = createNativeStackNavigator();
 export default function App() {
   return (
-    <View style={styles.container}>
-      <ImageBackground source={image} resizeMode="cover" style={styles.image}>
-        <LinearGradient
-          style={styles.container}
-          colors={["transparent", "rgba(0,0,0,0.8)"]}
-        >
-          <StatusBar style="auto" />
-          <View style={styles.field1}>
-            <Text style={styles.title}>EMIGRAR EN FAMILIA</Text>
-          </View>
-          <View style={styles.field2}>
-            <Pressable
-              style={styles.btn}
-              onPress={() => Alert.alert("Simple Button pressed")}
-            >
-              <Text style={styles.textBtn}>Ingresar</Text>
-            </Pressable>
-
-            <Text style={styles.text}>
-              No tienes cuenta todavía? Registrate{" "}
-              <Text
-                style={{ color: "#ED0101" }}
-                onPress={() => Linking.openURL("http://google.com")}
-              >
-                AQUI
-              </Text>
-            </Text>
-          </View>
-        </LinearGradient>
-      </ImageBackground>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Landing" component={Landing} />
+        <Stack.Screen name="Login" component={"#"} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 6,
-  },
-  title: {
-    color: "#FDFDFD",
-    fontWeight: "bold",
-    fontSize: 32,
-    textAlign: "center",
-    marginTop: 220,
-  },
-  image: {
-    flex: 1,
-    justifyContent: "center",
-  },
-  btn: {
-    color: "red",
-    backgroundColor: "#25C90A",
-    width: 300,
-    height: 58,
-    justifyContent: "center",
-    borderRadius: 40,
-  },
-  textBtn: {
-    textAlign: "center",
-    color: "#FDFDFD",
-    fontWeight: "bold",
-    fontSize: 18,
-  },
-  text: {
-    textAlign: "center",
-    color: "#FDFDFD",
-    fontWeight: "bold",
-    fontSize: 18,
-    marginTop: 40,
-  },
-  field1: {
-    flexDirection: "column",
-    flex: 5,
-  },
-  field2: {
-    flex: 2,
-    alignItems: "center",
-  },
-});
